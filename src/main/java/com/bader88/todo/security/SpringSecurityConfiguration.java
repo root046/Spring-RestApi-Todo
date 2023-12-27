718423ca15161c45e03c4407b5bff56b06d178b1
root046 <64629095+root046@users.noreply.github.com>
2023-12-27 15:17:09 +0300

    enhance and making isolated classes

--- src/main/java/com/bader88/todo/security/SpringSecurityConfiguration.java
+++ src/main/java/com/bader88/todo/security/SpringSecurityConfiguration.java
@@ -1,58 +1,17 @@
 package com.bader88.todo.security;
 
-import java.util.function.Function;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 public class SpringSecurityConfiguration {
-    //LDAP or Database
-    //In Memory
-
-    //InMemoryUserDetailsManager
-    //InMemoryUserDetailsManager(UserDetails... users)
-
-    @Bean
-    public InMemoryUserDetailsManager createUserDetailsManager() {
-
-        UserDetails userDetails1 = createNewUser("root", "0000");
-        UserDetails userDetails2 = createNewUser("x", "x");
-
-        return new InMemoryUserDetailsManager(userDetails1, userDetails2);
-    }
-
-    private UserDetails createNewUser(String username, String password) {
-        Function<String, String> passwordEncoder
-                = input -> passwordEncoder().encode(input);
-
-        UserDetails userDetails = User.builder()
-                .passwordEncoder(passwordEncoder)
-                .username(username)
-                .password(password)
-                .roles("USER","ADMIN")
-                .build();
-        return userDetails;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-    @Bean
+	@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 //    	//1: Response to preflight request doesn't pass access control check
 //		//2: basic auth
@@ -110,6 +69,5 @@ public class SpringSecurityConfiguration {
 //
 //        return http.build();
     }
-    
 
 }
