9ff60efddfb60133ec87828d228a74d58f544e80
root046 <64629095+root046@users.noreply.github.com>
2023-12-27 03:04:45 +0300

    Refactor Spring Security Configuration

--- src/main/java/com/bader88/todo/security/SpringSecurityConfiguration.java
+++ src/main/java/com/bader88/todo/security/SpringSecurityConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -55,59 +56,59 @@ public class SpringSecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 //    	//1: Response to preflight request doesn't pass access control check
 //		//2: basic auth
-//		return 
-//				http
-//					.authorizeHttpRequests(
-//						auth -> 
-//							auth
-//							.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-//							.anyRequest().authenticated()
-//						)
-//					.httpBasic(Customizer.withDefaults())
-//					.sessionManagement(
-//						session -> session.sessionCreationPolicy
-//						(SessionCreationPolicy.STATELESS))
-//						// .csrf().disable() Deprecated in SB 3.1.x
-//					.csrf(csrf -> csrf.disable()) // Starting from SB 3.1.x using Lambda DSL
-//			     // .csrf(AbstractHttpConfigurer::disable) // Starting from SB 3.1.x using Method Reference
-//					.build();
+		return 
+				http
+					.authorizeHttpRequests(
+						auth -> 
+							auth
+							.requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.OPTIONS, "/**")).permitAll()
+							.anyRequest().authenticated()
+						)
+					.httpBasic(Customizer.withDefaults())
+					.sessionManagement(
+						session -> session.sessionCreationPolicy
+						(SessionCreationPolicy.STATELESS))
+						// .csrf().disable() Deprecated in SB 3.1.x
+					.csrf(csrf -> csrf.disable()) // Starting from SB 3.1.x using Lambda DSL
+			     // .csrf(AbstractHttpConfigurer::disable) // Starting from SB 3.1.x using Method Reference
+					.build();
 
-//################## Old Trail ###############################		
-        http.authorizeHttpRequests(
-                auth -> auth
-//                	.requestMatchers(HttpMethod.OPTIONS,"/**").permitAll() 
-                	.anyRequest().authenticated()
-                	);
-        
-        http.formLogin(withDefaults());
-
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
-//        ########################################
+//################## Old Try(its work fine) ###############################		
 //        http.authorizeHttpRequests(
-//				auth -> auth.anyRequest().authenticated()
-//				);
-//		2) If a request is not authenticated, a popup is shown ask to login
-		http.httpBasic(withDefaults());
-		
-//		mangement session
-//		http.sessionManagement(
-//				session -> session.sessionCreationPolicy(
-//						SessionCreationPolicy.STATELESS
-//						)
-//				);
-		
-//		3) CSRF -> POST, PUT
-		// http.csrf().disable(); // Deprecated in SB 3.1.x
-        http.csrf(csrf -> csrf.disable()); // Starting from SB 3.1.x Using Lambda DSL
-        // OR
-        // http.csrf(AbstractHttpConfigurer::disable); // Starting from SB 3.1.x Using Method Reference
-//        -----------------------
-// Add this line to enable CORS configuration
-        http.cors(withDefaults());
-        
-
-        return http.build();
+//                auth -> auth
+////                	.requestMatchers(HttpMethod.OPTIONS,"/**").permitAll() 
+//                	.anyRequest().authenticated()
+//                	);
+//        
+//        http.formLogin(withDefaults());
+//
+//        http.csrf().disable();
+//        http.headers().frameOptions().disable();
+////        ########################################
+////        http.authorizeHttpRequests(
+////				auth -> auth.anyRequest().authenticated()
+////				);
+////		2) If a request is not authenticated, a popup is shown ask to login
+//		http.httpBasic(withDefaults());
+//		
+////		mangement session
+////		http.sessionManagement(
+////				session -> session.sessionCreationPolicy(
+////						SessionCreationPolicy.STATELESS
+////						)
+////				);
+//		
+////		3) CSRF -> POST, PUT
+//		// http.csrf().disable(); // Deprecated in SB 3.1.x
+//        http.csrf(csrf -> csrf.disable()); // Starting from SB 3.1.x Using Lambda DSL
+//        // OR
+//        // http.csrf(AbstractHttpConfigurer::disable); // Starting from SB 3.1.x Using Method Reference
+////        -----------------------
+//// Add this line to enable CORS configuration
+//        http.cors(withDefaults());
+//        
+//
+//        return http.build();
     }
     
 
